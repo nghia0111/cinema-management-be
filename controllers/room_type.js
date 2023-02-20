719e85b248c490721fb2d37ffdadee8d3b5d6335
nghia0111 <20521659@gm.uit.edu.vn>
2023-02-20 17:03:51 +0700

    add room type apis

--- controllers/room_type.js
+++ controllers/room_type.js
@@ -1,11 +1,11 @@
 const { validationResult } = require("express-validator");
-const Movie = require("../models/movie");
-const Genre = require("../models/genre");
+const RoomType = require("../models/room_type");
+const Room = require("../models/room");
 
 const { getRole } = require("../utils/roles");
 const { userRoles } = require("../constants");
 
-exports.createGenre = async (req, res, next) => {
+exports.createRoomType = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error(errors.array()[0].msg);
@@ -23,18 +23,18 @@ exports.createGenre = async (req, res, next) => {
       role != userRoles.OWNER
     ) {
       const error = new Error(
-        "Chỉ có quản lý, nhân viên hoặc chủ rạp mới được thêm thể loại phim"
+        "Chỉ có quản lý, nhân viên hoặc chủ rạp mới được thêm loại phòng"
       );
       error.statusCode = 401;
       return next(error);
     }
 
-    const genre = new Genre({
+    const type = new RoomType({
       name,
     });
-    await genre.save();
+    await type.save();
 
-    res.status(201).json({ message: "Thêm thể loại phim thành công" });
+    res.status(201).json({ message: "Thêm loại phòng thành công" });
   } catch (err) {
     const error = new Error(err.message);
     error.statusCode = 500;
@@ -42,7 +42,7 @@ exports.createGenre = async (req, res, next) => {
   }
 };
 
-exports.updateGenre = async (req, res, next) => {
+exports.updateRoomType = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error(errors.array()[0].msg);
@@ -51,7 +51,7 @@ exports.updateGenre = async (req, res, next) => {
     return next(error);
   }
 
-  const genreId = req.params.genreId;
+  const roomTypeId = req.params.roomTypeId;
 
   const { name } = req.body;
   try {
@@ -62,25 +62,25 @@ exports.updateGenre = async (req, res, next) => {
       role != userRoles.OWNER
     ) {
       const error = new Error(
-        "Chỉ có quản lý, nhân viên hoặc chủ rạp mới được chỉnh sửa thể loại phim"
+        "Chỉ có quản lý, nhân viên hoặc chủ rạp mới được chỉnh sửa loại phòng"
       );
       error.statusCode = 401;
       return next(error);
     }
 
-    const currentGenre = await Genre.findById(genreId);
-    if (!currentGenre) {
-      const err = new Error("Không tìm thấy thể loại phim");
+    const currentRoomType = await RoomType.findById(roomTypeId);
+    if (!currentRoomType) {
+      const err = new Error("Không tìm thấy loại phòng");
       err.statusCode = 406;
       next(err);
     }
 
-    currentGenre.name = name;
-    await currentGenre.save();
+    currentRoomType.name = name;
+    await currentRoomType.save();
 
     res.status(200).json({
-      message: "Chỉnh sửa thể loại phim thành công",
-      genre: currentGenre,
+      message: "Chỉnh sửa loại phòng thành công",
+      roomType: currentRoomType,
     });
   } catch (err) {
     const error = new Error(err.message);
@@ -89,8 +89,8 @@ exports.updateGenre = async (req, res, next) => {
   }
 };
 
-exports.deleteGenre = async (req, res, next) => {
-  const genreId = req.params.genreId;
+exports.deleteRoomType = async (req, res, next) => {
+  const roomTypeId = req.params.roomTypeId;
   try {
     const role = await getRole(req.accountId);
     if (
@@ -104,33 +104,22 @@ exports.deleteGenre = async (req, res, next) => {
       error.statusCode = 401;
       return next(error);
     }
-    const genre = await Genre.findById(genreId);
-    if (!genre) {
-      const error = new Error("Thể loại phim không tồn tại");
+    const roomType = await RoomType.findById(roomTypeId);
+    if (!roomType) {
+      const error = new Error("Loại phòng không tồn tại");
       error.statusCode = 406;
       return next(error);
     }
-    const movies = await Movie.find({ _id: { $in: genre.movies } });
-    let hasGenres = true;
-    for (let movie of movies) {
-      if (movie.genres.length < 2) {
-        hasGenres = false;
-        break;
-      }
-    }
-    if (!hasGenres) {
+    const rooms = await Room.find({ roomType: roomTypeId });
+    if (rooms) {
       const error = new Error(
-        "Phim phải gồm tối thiểu 2 thể loại để xóa thể loại này"
+        "Không thể xóa loại phòng do vẫn còn phòng thuộc loại này"
       );
       error.statusCode = 422;
       return next(error);
     }
-    for (let movie of movies) {
-      movie.genres.pull(genreId);
-      await movie.save();
-    }
-    await Movie.findByIdAndRemove(genreId);
-    res.status(200).json({ message: "Xoá thể loại phim thành công" });
+    await RoomType.findByIdAndRemove(roomTypeId);
+    res.status(200).json({ message: "Xoá loại phòng thành công" });
   } catch (err) {
     const error = new Error(err.message);
     error.statusCode = 500;
@@ -138,11 +127,11 @@ exports.deleteGenre = async (req, res, next) => {
   }
 };
 
-exports.getGenres = async (req, res, next) => {
+exports.getRoomTypes = async (req, res, next) => {
   try {
-    const genres = await Genre.find();
+    const roomTypes = await RoomType.find();
 
-    res.status(200).json({ genres });
+    res.status(200).json({ roomTypes });
   } catch (err) {
     const error = new Error(err.message);
     error.statusCode = 500;
--- models/room_type.js
+++ models/room_type.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const roomTypeSchema = new Schema({
-    name: {
-        type: String,
-        require: true
-    }
-})
+  name: {
+    type: String,
+    require: true,
+  },
+});
 
 module.exports = mongoose.model("RoomType", roomTypeSchema);
