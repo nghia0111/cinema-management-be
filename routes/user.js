589194872c322ada0d1c12e2cc6a2dc0fed00844
nghia0111 <20521659@gm.uit.edu.vn>
2023-03-28 23:59:58 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -9,22 +9,29 @@ const isAuth = require("../middlewares/is-auth");
 const userValidation = [
   body("name", "Vui lòng cung cấp tên người dùng").trim().notEmpty(),
   body("address", "Vui lòng cung cấp địa chỉ").trim().notEmpty(),
-  body("email")
-    .isEmail()
-    .withMessage("Email không hợp lệ")
-    .custom((value, { req }) => {
-      return User.findOne({ email: value }).then((userDoc) => {
-        if (userDoc) {
-          return Promise.reject("Email đã tồn tại, vui lòng chọn email khác");
-        }
-      });
-    })
-    .normalizeEmail(),
   body("phone", "Số điện thoại không hợp lệ").isMobilePhone("vi-VN"),
   body("birthday", "Ngày sinh không hợp lệ").isISO8601().toDate(),
 ];
 
-router.post("/users", isAuth, userValidation, userController.createUser);
+router.post(
+  "/users",
+  isAuth,
+  [
+    ...userValidation,
+    body("email")
+      .isEmail()
+      .withMessage("Email không hợp lệ")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("Email đã tồn tại, vui lòng chọn email khác");
+          }
+        });
+      })
+      .normalizeEmail(),
+  ],
+  userController.createUser
+);
 
 router.put("/users/:userId", isAuth, userValidation, userController.updateUser);
 
