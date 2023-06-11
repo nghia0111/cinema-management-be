3dae5358dde1dcc86fc7e9a897105d974b0e332e
nghia0111 <20521659@gm.uit.edu.vn>
2023-06-11 11:24:17 +0700

    Update report.js

--- controllers/report.js
+++ controllers/report.js
@@ -8,10 +8,7 @@ const { userRoles } = require("../constants");
 exports.getDashboardData = async (req, res, next) => {
   try {
     const role = await getRole(req.accountId);
-    if (
-      role != userRoles.MANAGER &&
-      role != userRoles.OWNER
-    ) {
+    if (role != userRoles.MANAGER && role != userRoles.OWNER) {
       const error = new Error(
         "Chỉ có quản lý hoặc chủ rạp mới được xem báo cáo"
       );
@@ -20,17 +17,41 @@ exports.getDashboardData = async (req, res, next) => {
     }
     const data = {};
     const currentDate = new Date();
-    currentDate.setHours(0,0,0,0);
+    currentDate.setHours(0, 0, 0, 0);
     const nextDate = new Date();
     nextDate.setHours(24, 0, 0, 0);
-    const transactions = await Transaction.find({createdAt: {$gte: currentDate, $lt: new Date()}});
-    data.revenue = transactions.reduce((accumulator, transaction) => accumulator + transaction.totalPrice, 0);
-    const show_times = await ShowTime.find({startTime: {$gte: currentDate, $lt: nextDate}});
-    const showTimeIds = show_times.map(st => st._id);
-    const tickets = await Ticket.find({showTime: {$in: showTimeIds}});
-    data.soldTickets = tickets.filter(ticket => ticket.isBooked).length;
+    const transactions = await Transaction.find({
+      createdAt: { $gte: currentDate, $lt: new Date() },
+    });
+    data.revenue = transactions.reduce(
+      (accumulator, transaction) => accumulator + transaction.totalPrice,
+      0
+    );
+    const show_times = await ShowTime.find({
+      startTime: { $gte: currentDate, $lt: nextDate },
+    });
+    const showTimeIds = show_times.map((st) => st._id);
+    const tickets = await Ticket.find({ showTime: { $in: showTimeIds } });
+    data.soldTickets = tickets.filter((ticket) => ticket.isBooked).length;
     data.remainingTickets = tickets.length - data.soldTickets;
-    const recentTransactions = await Transaction.find().sort({createdAt: -1}).limit(5);
+    const recentTransactions = await Transaction.find()
+      .sort({ createdAt: -1 })
+      .limit(5)
+      .select("-items")
+      .populate("staff", "name")
+      .populate("customer", "name");
+    for (let i = 0; i < recentTransactions.length; i++) {
+      const ticket = await Ticket.findById(
+        recentTransactions[i].tickets[0]
+      ).populate({
+        path: "showTime",
+        select: "movie",
+        populate: { path: "movie", select: "name" },
+      });
+      let transaction = recentTransactions[i].toJSON();
+      transaction.movie = ticket.showTime.movie.name;
+      recentTransactions[i] = { ...transaction };
+    }
     data.recentTransactions = recentTransactions;
     res.status(200).json({ data });
   } catch (err) {
