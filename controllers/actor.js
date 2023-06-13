0c4f1bcace7bee002d600c9799c76380d5957ef4
nghia0111 <20521659@gm.uit.edu.vn>
2023-06-13 21:27:56 +0700

    add nations api

--- controllers/actor.js
+++ controllers/actor.js
@@ -177,3 +177,18 @@ exports.getActorBySlug = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getActorNations = async (req, res, next) => {
+  try {
+    const actors = await Actor.find();
+    const nations = [];
+    for(let actor of actors){
+      if(!nations.includes(actor.nation)) nations.push(actor.nation);
+    }
+    res.status(200).json({ nations });
+  } catch (err) {
+    const error = new Error(err.message);
+    error.statusCode = 500;
+    next(error);
+  }
+};
