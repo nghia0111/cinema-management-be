c5312bbee73cc3f2c32d016cc2ac77d009aef343
nghia0111 <20521659@gm.uit.edu.vn>
2023-06-16 02:00:16 +0700

    add daily report api

--- controllers/report.js
+++ controllers/report.js
@@ -3,7 +3,7 @@ const Ticket = require("../models/ticket");
 const ShowTime = require("../models/show_time");
 const Movie = require("../models/movie");
 
-const { getRole, getLocalDate } = require("../utils/service");
+const { getRole, getLocalDate, getNextDate } = require("../utils/service");
 const { userRoles } = require("../constants");
 
 exports.getDashboardData = async (req, res, next) => {
@@ -35,7 +35,10 @@ exports.getDashboardData = async (req, res, next) => {
     const tickets = await Ticket.find({ showTime: { $in: showTimeIds } });
     data.soldTickets = tickets.filter((ticket) => ticket.isBooked).length;
     data.remainingTickets = tickets.length - data.soldTickets;
-    const movies = await Movie.find({endDay: {$gt: getLocalDate()}, premiereDay: {$lte: getLocalDate()}}).countDocuments();
+    const movies = await Movie.find({
+      endDay: { $gt: getLocalDate() },
+      premiereDay: { $lte: getLocalDate() },
+    }).countDocuments();
     data.onGoingMovies = movies;
     const recentTransactions = await Transaction.find()
       .sort({ createdAt: -1 })
@@ -54,8 +57,8 @@ exports.getDashboardData = async (req, res, next) => {
       let transaction = recentTransactions[i].toJSON();
       transaction.movie = {
         name: ticket.showTime.movie.name,
-        thumbnail: ticket.showTime.movie.thumbnail
-      }
+        thumbnail: ticket.showTime.movie.thumbnail,
+      };
       recentTransactions[i] = { ...transaction };
     }
     data.recentTransactions = recentTransactions;
@@ -67,3 +70,69 @@ exports.getDashboardData = async (req, res, next) => {
   }
 };
 
+exports.getDailyReport = async (req, res, next) => {
+  try {
+    const role = await getRole(req.accountId);
+    if (role != userRoles.MANAGER && role != userRoles.OWNER) {
+      const error = new Error(
+        "Chỉ có quản lý hoặc chủ rạp mới được xem báo cáo"
+      );
+      error.statusCode = 401;
+      return next(error);
+    }
+    const { date } = req.body;
+    const showTimes = await ShowTime.find({
+      startTime: { $gte: date, $lt: getNextDate(date) },
+    }).populate("movie", "name thumbnail");
+    const data = {};
+    const movies = {};
+    const items = {};
+    const showTimeIds = [];
+    const ticketIds = [];
+    for (let showTime of showTimes) {
+      showTimeIds.push(showTime._id.toString());
+      movies[showTime.movie._id] = {
+        name: showTime.movie.name,
+        thumbnail: showTime.movie.thumbnail,
+        soldTicketQuantity: 0,
+        ticketRevenue: 0,
+      };
+    }
+    const soldTickets = await Ticket.find({
+      showTime: { $in: showTimeIds },
+      isBooked: true,
+    }).populate("showTime", "movie");
+    for (let ticket of soldTickets) {
+      ticketIds.push(ticket._id.toString());
+      movies[ticket.showTime.movie].soldTicketQuantity += 1;
+      movies[ticket.showTime.movie].ticketRevenue += ticket.price;
+    }
+    data.movies = movies;
+    const transactions = await Transaction.find({
+      createdAt: { $lt: getNextDate(date) },
+    }).populate({ path: "items.id", select: "-image" });
+    const expectedTransactions = transactions.filter((tran) =>
+      ticketIds.includes(tran.tickets[0].toString())
+    );
+    for (let transaction of expectedTransactions) {
+      for (let item of transaction.items) {
+        if (!items.hasOwnProperty(item.id._id.toString())) {
+          items[item.id._id.toString()] = {
+            name: item.id.name,
+            quantity: item.quantity,
+            totalPrice: item.id.price * item.quantity,
+          };
+        } else {
+          items[item.id._id.toString()].quantity += item.quantity;
+          items[item.id._id.toString()].totalPrice += item.id.price * item.quantity;
+        }
+      }
+    }
+    data.items = items;
+    res.status(200).json({ data });
+  } catch (err) {
+    const error = new Error(err.message);
+    error.statusCode = 500;
+    next(error);
+  }
+};
--- routes/report.js
+++ routes/report.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
 
 const reportController = require("../controllers/report");
 const isAuth = require("../middlewares/is-auth");
+
+const reportValidation = [
+  body("date", "Ngày không hợp lệ").isISO8601().toDate(),
+];
 
 router.get(
   "/dashboard",
   isAuth,
   reportController.getDashboardData
 );
+
+router.post("/daily-report", isAuth, reportValidation, reportController.getDailyReport)
 
 module.exports = router;
